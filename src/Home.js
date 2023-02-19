df26f1d7a5107cf540f368f112d002b76c30a9df
Tarun Mandhan <mandhantarun@gmail.com>
2023-02-19 18:56:29 +0530

    .

--- src/Home.js
+++ src/Home.js
@@ -24,43 +24,22 @@ function Home() {
             id="12455542"
             tittle="Lenovo IdeaPad Gaming 3 Intel Core i5 11th Gen 15.6 inch (39.62cm) FHD IP"
             price={35000}
-            rating={5}
+            rating={3}
             image="https://m.media-amazon.com/images/I/41bufC9IzpL._AC_SR400,600_.jpg"
           />
           <Product
             id="12455542"
             tittle="Lenovo IdeaPad Gaming 3 Intel Core i5 11th Gen 15.6 inch (39.62cm) FHD IP"
             price={35000}
-            rating={5}
+            rating={2}
             image="https://m.media-amazon.com/images/I/41bufC9IzpL._AC_SR400,600_.jpg"
           />
           <Product
             id="12455542"
             tittle="Lenovo IdeaPad Gaming 3 Intel Core i5 11th Gen 15.6 inch (39.62cm) FHD IP"
             price={35000}
-            rating={5}
-            image="https://m.media-amazon.com/images/I/41bufC9IzpL._AC_SR400,600_.jpg"
-          />
-          <Product
-            id="12455542"
-            tittle="Lenovo IdeaPad Gaming 3 Intel Core i5 11th Gen 15.6 inch (39.62cm) FHD IP"
-            price={35000}
-            rating={5}
-            image="https://m.media-amazon.com/images/I/41bufC9IzpL._AC_SR400,600_.jpg"
-          />
-          <Product
-            id="12455542"
-            tittle="Lenovo IdeaPad Gaming 3 Intel Core i5 11th Gen 15.6 inch (39.62cm) FHD IP"
-            price={35000}
-            rating={5}
-            image="https://m.media-amazon.com/images/I/41bufC9IzpL._AC_SR400,600_.jpg"
-          />
-          <Product
-            id="12455542"
-            tittle="Lenovo IdeaPad Gaming 3 Intel Core i5 11th Gen 15.6 inch (39.62cm) FHD IP"
-            price={35000}
-            rating={5}
-            image="https://m.media-amazon.com/images/I/41bufC9IzpL._AC_SR400,600_.jpg"
+            rating={4}
+            image="https://m.media-amazon.com/images/I/41ATQlB-gXL._AC_SY200_.jpg"
           />
         </div>
         {/* Product */}
