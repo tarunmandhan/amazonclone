ca172002bf460e0a970c6278f0b91017d27e3753
Tarun Mandhan <mandhantarun@gmail.com>
2023-02-20 15:39:06 +0530

    add dynamic count & useStateValue()  header basket

--- src/Header.js
+++ src/Header.js
@@ -3,8 +3,11 @@ import "./Header.css";
 import SearchIcon from "@material-ui/icons/Search";
 import ShoppingBasketIcon from "@material-ui/icons/ShoppingBasket";
 import { Link } from "react-router-dom";
+import { useStateValue } from "./StateProvider";
 
 function Header() {
+  const [{ basket, user }, dispatch] = useStateValue();
+
   return (
     <>
       <div className="header">
@@ -54,7 +57,7 @@ function Header() {
               <ShoppingBasketIcon />
               {/* Number of items in the basket  */}
               <span className="header__optionLineTwo header__basketCount">
-                0
+                {basket?.length}
               </span>
             </div>
           </Link>
