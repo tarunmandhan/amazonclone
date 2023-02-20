c39f158d896b4d32819d36b7192be7f5884fc727
Tarun Mandhan <mandhantarun@gmail.com>
2023-02-20 16:01:02 +0530

    functionality-onClick  to add product to basket

--- src/Product.js
+++ src/Product.js
@@ -1,12 +1,28 @@
 import React from "react";
 import "./Product.css";
+import { useStateValue } from "./StateProvider";
 
-function Product({ id, tittle, image, price, rating }) {
+function Product({ id, title, image, price, rating }) {
+  const [{ basket }, dispatch] = useStateValue();
+
+  const addToBasket = () => {
+    // dispatch the item into the data layer
+    dispatch({
+      type: "ADD_TO_BASKET",
+      item: {
+        id: id,
+        title: title,
+        image: image,
+        price: price,
+        rating: rating,
+      },
+    });
+  };
   return (
     <>
       <div className="product">
         <div className="product__info">
-          <p>{tittle}</p>
+          <p>{title}</p>
           <p className="product__price">
             <small>₨</small>
             <strong>{price}</strong>
@@ -20,7 +36,7 @@ function Product({ id, tittle, image, price, rating }) {
           </div>
         </div>
         <img src={image} alt="" />
-        <button>Add to basket </button>
+        <button onClick={addToBasket}>Add to basket </button>
       </div>
     </>
   );
