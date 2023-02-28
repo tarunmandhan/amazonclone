12c3f01801158392e22193ceb5e61e2d0c0c6d7d
Tarun Mandhan <mandhantarun@gmail.com>
2023-02-28 23:30:50 +0530

    firebase

--- src/App.js
+++ src/App.js
@@ -1,12 +1,37 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import Header from "./Header";
 import Home from "./Home";
 import Checkout from "./Checkout";
 import Login from "./Login";
+import { auth } from "./firebase";
+import { useStateValue } from "./StateProvider";
+
 
 function App() {
+  const [{ basket }, dispatch] = useStateValue();
+
+  //useEffect  <<<<<<<<<<<<  POWERFULL
+  // Piece of code whihc runs based on a given condition
+
+  useEffect(() => {
+    auth.onAuthStateChanged((authUser) => {
+      if (authUser) {
+        // user is logged in..
+        dispatch({
+          type: "SET_USER",
+          user: authUser,
+        });
+      } else {
+        // user is logged out..
+        dispatch({
+          type: "SET_USER",
+          user: null,
+        });
+      }
+    });
+  }, []);
   return (
     <Router>
       <div className="app">
